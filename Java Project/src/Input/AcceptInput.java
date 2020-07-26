ef5fff6ca18eb4872d904c1f8cccc6b5f366c6cf
DHANYAKRISHNA <DHANYAKRISHNA@Dhanya>
2020-07-26 21:03:15 +0530

    My First GIT Project

---
+++ Java Project/src/Input/AcceptInput.java
@@ -0,0 +1,24 @@
+package Input;
+
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class AcceptInput {
+
+	public static void main (String [] args) throws IOException{
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Learning Java from?");
+		String website = userInput.nextLine();
+		userInput.close();
+		System.out.println("I am learning Java from "+ website);
+		
+		//Type 2
+		
+		BufferedReader myReader = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Input your site name :");
+		String mySite = myReader.readLine();
+		System.out.println("Site name is : "+ mySite); 
+	}
+}
