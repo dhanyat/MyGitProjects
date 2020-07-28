a5881bc3dd861dc012566b01c342c74d2a28799a
DHANYAKRISHNA <DHANYAKRISHNA@Dhanya>
2020-07-28 23:27:09 +0800

    Date program added

---
+++ Java Project/src/Input/dateDemo.java
@@ -0,0 +1,20 @@
+package Input;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class dateDemo {
+	
+	public static void main(String[] args){
+		
+		Date d= new Date();
+		//t = d.getTime();
+		SimpleDateFormat sdf = new SimpleDateFormat("hh_mm_ss");
+		String time = sdf.format(d);
+		System.out.println(time);
+		
+		System.out.println(d);
+		
+	}
+
+}
